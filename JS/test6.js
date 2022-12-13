90ff1a06541e9d23df6f160c66814820ef18dc40
Albert7747 <57909560+Albert7747@users.noreply.github.com>
2022-12-14 02:12:27 +0800

    Update test6.js

--- JS/test6.js
+++ JS/test6.js
@@ -6,7 +6,7 @@ const myLink = document.querySelector(".change_web");
 // // //監聽事件
 submit.addEventListener("click", function() {
     if(txt.value=="MELODY" || txt.value=="melody"||txt.value=="Melody"){
-        myLink.setAttribute("href","/Puzzle_fourth_result.html");
+        myLink.setAttribute("href","https://albert7747.github.io/puzzle_game.github.io/Puzzle_fourth_result.html");
     }else{
         alert("答錯了，再想想看吧！")
     }
