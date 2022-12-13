d65de0d1427ae95f3b94c33131fcdb838927f777
Albert7747 <57909560+Albert7747@users.noreply.github.com>
2022-12-14 02:11:53 +0800

    Update test5.js

--- JS/test5.js
+++ JS/test5.js
@@ -11,33 +11,33 @@ submit.addEventListener("click", function () {
     if(txt_1.value == "那些年"){
       if(txt_2.value == "等一個人"){
         if(txt_3.value == "讓我為你唱情歌"){
-            myLink.setAttribute("href","/Puzzle_third_result.html");
+            myLink.setAttribute("href","https://albert7747.github.io/puzzle_game.github.io/Puzzle_third_result.html");
         }else{ alert("答錯了，再想想看吧！"); }
       }else if(txt_2.value == "讓我為你唱情歌"){
         if(txt_3.value == "等一個人"){
-            myLink.setAttribute("href","/Puzzle_third_result.html");}else{ alert("答錯了，再想想看吧！"); }
+            myLink.setAttribute("href","https://albert7747.github.io/puzzle_game.github.io/Puzzle_third_result.html");}else{ alert("答錯了，再想想看吧！"); }
       }else{ alert("答錯了，再想想看吧！"); }
      
     }
     if(txt_1.value == "等一個人"){
       if(txt_2.value == "那些年"){
         if(txt_3.value == "讓我為你唱情歌"){
-            myLink.setAttribute("href","/Puzzle_third_result.html");
+            myLink.setAttribute("href","https://albert7747.github.io/puzzle_game.github.io/Puzzle_third_result.html");
         }else{ alert("答錯了，再想想看吧！"); }
       }else if(txt_2.value == "讓我為你唱情歌"){
         if(txt_3.value == "那些年"){
-            myLink.setAttribute("href","/Puzzle_third_result.html");}else{ alert("答錯了，再想想看吧！"); }
+            myLink.setAttribute("href","/https://albert7747.github.io/puzzle_game.github.ioPuzzle_third_result.html");}else{ alert("答錯了，再想想看吧！"); }
       }else{ alert("答錯了，再想想看吧！"); }
      
     }
     if(txt_1.value == "讓我為你唱情歌"){
       if(txt_2.value == "等一個人"){
         if(txt_3.value == "那些年"){
-            myLink.setAttribute("href","/Puzzle_third_result.html");
+            myLink.setAttribute("href","https://albert7747.github.io/puzzle_game.github.io/Puzzle_third_result.html");
         }else{ alert("答錯了，再想想看吧！"); }
       }else if(txt_2.value == "那些年"){
         if(txt_3.value == "等一個人"){
-            myLink.setAttribute("href","/Puzzle_third_result.html");}else{ alert("答錯了，再想想看吧！"); }
+            myLink.setAttribute("href","https://albert7747.github.io/puzzle_game.github.io/Puzzle_third_result.html");}else{ alert("答錯了，再想想看吧！"); }
       }else{ alert("答錯了，再想想看吧！"); }
      
     }
